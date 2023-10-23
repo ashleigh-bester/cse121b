bb15805a5f170bc078b5cf3e438135198b547201
ashleigh-bester <ashleighbester8@gmail.com>
2023-10-23 22:40:58 +0200

    profile

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -2,22 +2,22 @@
 
 /* Profile Object  */
 let myProfile = {
-    name: 'Ashleigh Bester'
-    photo: 'images\profile pic.jpg'
+    name: 'Ashleigh Bester',
+    photo: 'images\profile pic.jpg',
     favoriteFoods: [
         'Potatoes', 
         'Donuts', 
         'Sushi', 
         'Pasta', 
         'Strawberries'
-    ]
+    ],
     hobbies: [
-        'hiking',
-        'game drives',
-        'animal rehabilitation',
-        'ukulele',
-        'public speaking'
-    ]
+        'Hiking',
+        'Game drives',
+        'Animal rehabilitation',
+        'Ukulele',
+        'Public speaking'
+    ],
     placesLived: ['']
 };
 
@@ -26,19 +26,19 @@ let myProfile = {
 /* Populate Profile Object with placesLive objects */
 myProfile.placesLived.push(
     {
-        place: 'Gauteng, South Africa'
+        place: 'Gauteng, South Africa',
         length: '11 years'
     },
     {
-        place: 'Melbourne, Australia'
+        place: 'Melbourne, Australia',
         length: '1 year'
     },
     {
-        place: 'Eastern Cape, South Africa'
+        place: 'Eastern Cape, South Africa',
         length: '2-3 months'
     },
     {
-        place: 'Mpumalanga, South Africa'
+        place: 'Mpumalanga, South Africa',
         length: 'Almost 7 years'
     }
 );
@@ -51,6 +51,7 @@ myProfile.placesLived.push(
 document.querySelector('#name').textContent = myProfile.name;
 
 /* Photo with attributes */
+const imageElement = document.querySelector('#home picture img');
 imageElement.setAttribute('src', myProfile.photo);
 imageElement.setAttribute('alt', `Profile image of ${myProfile.name}`);
 
@@ -58,13 +59,24 @@ imageElement.setAttribute('alt', `Profile image of ${myProfile.name}`);
 myProfile.favoriteFoods.forEach(food => {
     let li = document.createElement('li');
     li.textContent = food;
-    document.querySelector('favorite-foods').appendChild(li);
+    document.querySelector('#favorite-foods').appendChild(li);
 }
     )
 
 /* Hobbies List */
-
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement('li');
+    li.textContent = hobby;
+    document.querySelector('#hobbies').appendChild(li);
+}
+    )
 
 /* Places Lived DataList */
+let dt = document.createElement('dt');
+dt.innerHTML = placesLived.place;
+document.querySelector('#places-lived').appendChild(dt);
 
+let dd = document.createElement('dd');
+dd.innerHTML = placesLived.length;
+document.querySelector('#places-lived').appendChild(dd);
 
